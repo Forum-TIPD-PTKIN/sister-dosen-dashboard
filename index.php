172cfa8ef2cc99e42724b84a71cd79de96a1e647
Wildan Tea <wildantea@192.168.1.2>
2025-08-20 23:07:52 +0700

    add htaccess

--- index.php
+++ index.php
@@ -3,31 +3,25 @@ session_start();
 include "backend/inc/config.php";
 include "includes/config.php";
 
+// Get the first URI segment
 $uri0 = uri_segment(0);
 $uri1 = uri_segment(1);
-
-// Kalau akses ke /backend, lempar ke backend/index.php
-if ($uri0 === 'backend') {
-    include __DIR__ . "/backend/index.php";
-    exit;
-}
-
-switch ($uri0) {
-    case '':
-        include "dashboard.php";
-        break;
-    case 'detail':
+switch (uri_segment(0)) {
+	case '':
+		include "dashboard.php";
+		break;
+	case 'detail':
         $id_sdm = $uri1;
         $dosen = $db->fetchSingleRow('view_sdm', 'id_sdm', $id_sdm);
         if (!$dosen) {
             include "not_found.php";
             exit;
         }
-        include 'detail_dosen.php';
-        break;
-    default:
-        if ($uri0 != "") {
-            include "not_found.php";
-        }
-        break;
+		include 'detail_dosen.php';
+		break;
+	default :
+	if (uri_segment(0)!="") {
+		include "not_found.php";
+	}
+	break;
 }

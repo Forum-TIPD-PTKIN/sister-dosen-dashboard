067eeb8734a7404793489c61be864451e9f56d55
Candra <candraadiputra@gmail.com>
2026-07-15 17:16:06 +0700

    Update detail_dosen.php
    Hapus tampilan NIDN dan NUPTK dari halaman detail dosen

--- detail_dosen.php
+++ detail_dosen.php
@@ -344,8 +344,8 @@ foreach ($trend as $tahun => $data) {
 
                 // Get detail data
                 $detail = $db->fetchSingleRow('view_sdm_details', 'id_sdm', $id_sdm);
-                $nidn = $detail->nidn ?? '-';
-                $nuptk = $detail->nuptk ?? '-';
+               // $nidn = $detail->nidn ?? '-';
+               // $nuptk = $detail->nuptk ?? '-';
                 // $nip = $detail->nip ?? '-';
                 $home_base = $detail->home_base ?? ($dosen->home_base ?? '-');
 
